4f6a3bebb1709cafd171a54071b9568181b9fc2a
Luis A. Antúnez <67904662+l-antunez@users.noreply.github.com>
2025-02-10 16:00:22 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/ec2-user/app
+
+# Si usas nvm para gestionar Node.js, asegurarte de cargarlo
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Cargar nvm
+
+# Asegurarse de que npm esté en el PATH
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/$(nvm current)/bin
+
+# Instalar dependencias
 npm install
+
